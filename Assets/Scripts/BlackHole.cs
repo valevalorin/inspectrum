1cdb2994412ac646662148f0f4ffa3f01ba5629d
David Nguyen <dnguyenchevy@live.com>
2015-02-15 15:20:46 -0600

    init black_hole and scene

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour {
+	public GameObject left;
+	public GameObject right;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
