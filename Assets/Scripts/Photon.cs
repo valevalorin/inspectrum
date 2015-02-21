4264bfcc6980c41281a5d3685f3a2855dd4458cc
dnguyenchevy <dnguyenchevy@live.com>
2015-02-20 19:32:24 -0600

    Added game UI, remocked scene

--- Assets/Scripts/Photon.cs
+++ Assets/Scripts/Photon.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Photon : MonoBehaviour {
 	public GameManager GM;
 
+	//DELETE this constant later, just for testing photon to move left/right
+	const float SPEED = 100;
 
 	// Use this for initialization
 	void Start () {
-		rigidbody2D.velocity = GM.PhotonSpeed;
+		//rigidbody2D.velocity = GM.PhotonSpeed;
+		rigidbody2D.AddForce (Vector3.left * SPEED);
 	}
 	
 	// Update is called once per frame
