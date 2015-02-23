ab5528646971aa98a549f9830e44fd5a3026568b
ClaytonG <cmrg9175@gmail.com>
2015-02-23 12:28:53 -0600

    Edited the ParticleManager script and added particles to the menu screen.

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -4,27 +4,52 @@ using System.Collections;
 public class ParticleManager : MonoBehaviour {
 
 	public float trackSpeed;
+	public float menuSpeed;
 	
 	public ParticleSystem nearSystem;
 	public ParticleSystem midSystem;
 	public ParticleSystem farSystem;
+
+	private PhotonManager photonManager;
 	
 	private const float NEAR_SIZE = 0.3f;
 	private const float MID_SIZE = 0.15f;
 	private const float FAR_SIZE = 0.075f;
-	
-	// Use this for initialization
-	void Start () {
-		
+	private const int MENU = 0;
+
+	void Awake(){
+
 		//Set the size for each particle system
 		nearSystem.startSize = NEAR_SIZE;
 		midSystem.startSize = MID_SIZE;
 		farSystem.startSize = FAR_SIZE;
 		
 		//Set the speed for each particle system
-		nearSystem.startSpeed = trackSpeed;
-		midSystem.startSpeed = nearSystem.startSpeed / 2f;
-		farSystem.startSpeed = midSystem.startSpeed / 2f;
+		if(Application.loadedLevel == MENU){
+			nearSystem.startSpeed = menuSpeed;
+			nearSystem.startLifetime = Screen.width / nearSystem.startSpeed;
+
+			midSystem.startSpeed = nearSystem.startSpeed / 2f;
+			midSystem.startLifetime = Screen.width / midSystem.startSpeed;
+
+			farSystem.startSpeed = midSystem.startSpeed / 2f;
+			farSystem.startLifetime = Screen.width / farSystem.startSpeed;
+		}
+		else{
+			nearSystem.startSpeed = trackSpeed;
+			nearSystem.startLifetime = Screen.width / nearSystem.startSpeed;
+
+			midSystem.startSpeed = nearSystem.startSpeed / 2f;
+			midSystem.startLifetime = Screen.width / midSystem.startSpeed;
+
+			farSystem.startSpeed = midSystem.startSpeed / 2f;
+			farSystem.startLifetime = Screen.width / farSystem.startSpeed;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
 	}
 	
 	// Update is called once per frame
