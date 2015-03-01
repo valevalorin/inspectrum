f7f68336a01c76f0e51b5c542b1d7f1e6bab82b7
valevalorin <S510243@NW34974.nwmissouri.edu>
2015-03-01 14:39:12 -0600

    Pause menu complete, destroy wall implemented.

---
+++ Assets/DestroyWall.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWall : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(other.gameObject);
+	}
+}
---
+++ Assets/PauseButtons.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButtons : MonoBehaviour {
+
+	public GameManager GM;
+
+	void Start()
+	{
+		GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+	}
+
+	public void ResumeGame()
+	{
+		GM.IsPaused = !GM.IsPaused;
+	}
+
+	public void RestartGame()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void QuitToMenu()
+	{
+		GameObject.Destroy(GM.data.gameObject);
+		Application.LoadLevel(0);
+	}
+
+	public void QuitToDesktop()
+	{
+		Application.Quit();
+	}
+}
