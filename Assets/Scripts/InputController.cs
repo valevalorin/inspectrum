cdbf159d1e3fc4db583296e7eea96b653404fa38
valevalorin <S510243@NW34974.nwmissouri.edu>
2015-03-04 12:15:19 -0600

    Comments and cleanup on inputcontroller and photonmanager

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 
 public class InputController : MonoBehaviour
 {
+	//How many frames the player has to enter combinations. Setable in inspector
 	public int InputFrames;
-	public BlackHoleTrigger left_zone;
-	public BlackHoleTrigger right_zone;
+
 	public PhotonManager PM;
 	public GameManager GM;
 
+	//The shortest and longest distance the photon can be away from the photon manager and it count as a hit.
 	public float entryZoneEnd;
 	public float entryZoneBegin;
 
+	//Input window control variables
 	private bool inputActive = false;
 	private int activeInputCounter = 0;
 	private List<InputColor> activeColors;
@@ -20,6 +22,7 @@ public class InputController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		//Set empty color inputs; get references to PM and GM
 		activeColors = new List<InputColor>();
 		PM = (PhotonManager) GameObject.FindGameObjectWithTag("PhotonManager").GetComponent<PhotonManager>();
 		GM = (GameManager) GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
@@ -28,17 +31,21 @@ public class InputController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		//Handle pause menu first
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
 			GM.IsPaused = !GM.IsPaused;
 		}
 
+		//Get color keys currently pressed.
 		bool red = Input.GetKeyDown(KeyCode.A);
 		bool blue = Input.GetKeyDown(KeyCode.S);
 		bool yellow = Input.GetKeyDown(KeyCode.D);
+
 		float distance;
 		PhotonData currentPhoton;
 
+		//if there's a photon to process put it in current photon and find out how far it is from the manager; else exit method
 		if(PM.PhotonQueue.Count > 0)
 		{
 			currentPhoton = (PhotonData) PM.PhotonQueue.Peek();
@@ -47,7 +54,7 @@ public class InputController : MonoBehaviour
 		else
 			return;
 
-
+		//First check if the current photon is past the entry zone; if it is count it as a miss
 		if(distance > entryZoneEnd)
 		{
 			Debug.Log ("MISS!: "+distance);
@@ -56,12 +63,14 @@ public class InputController : MonoBehaviour
 			PM.PhotonQueue.Dequeue();
 		}
 
+		//Else check if input has become active
 		if(!inputActive && (red || blue || yellow))
 		{
 			inputActive = true;
 			activeInputCounter = InputFrames;
 		}
 
+		//If input is active add color keys to input list if they are not already there
 		if(inputActive)
 		{
 			if(red && !activeColors.Contains(InputColor.RED))
@@ -74,24 +83,29 @@ public class InputController : MonoBehaviour
 				activeColors.Add(InputColor.YELLOW);
 		}
 
+		//If the input window has ended
 		if(inputActive && activeInputCounter == 0)
 		{
 			InputColor input = determineColor(activeColors.Contains(InputColor.RED), activeColors.Contains(InputColor.BLUE), activeColors.Contains(InputColor.YELLOW));
 
+			//If photon is inside the entry zone and the right color, score hit
 			if(distance > entryZoneBegin && distance <= entryZoneEnd && input == currentPhoton.color)
 			{
 				Debug.Log ("HIT!");
 				GM.Score();
+
+				//Change velocity to save photon from blackhole and dequeue them.
 				currentPhoton.self.gameObject.rigidbody2D.velocity = new Vector2 (GM.PhotonSpeed, RandomUpOrDown() * GM.PhotonSpeed);
 				PM.PhotonQueue.Dequeue();
 
 			}
 			else if((distance < entryZoneBegin && distance > (entryZoneBegin - 1.3)) || (distance > entryZoneBegin && distance <= entryZoneEnd && input != currentPhoton.color))
 			{
+				//If photon is partially but not fully in the zone OR in the zone and the wrong color score miss
 				Debug.Log ("MISS!: "+distance);
-				currentPhoton.self.tag = "DeadPhoton";
+				currentPhoton.self.tag = "DeadPhoton"; //Used to make sure the photon isn't counted for other interactions
 				GM.Miss();
-				PM.PhotonQueue.Dequeue();
+				PM.PhotonQueue.Dequeue(); //Dequeue
 			}
 
 
@@ -100,10 +114,12 @@ public class InputController : MonoBehaviour
 			inputActive = false;
 		}
 
+		//Countdown input frames
 		if(inputActive)
 			activeInputCounter--;
 	}
 
+	//Determines the color input by the player
 	private InputColor determineColor(bool red, bool blue, bool yellow)
 	{	
 		if(red && !(blue || yellow))
@@ -124,6 +140,7 @@ public class InputController : MonoBehaviour
 		return InputColor.NONE;
 	}
 
+	//Used to randomly send the photon up or down on score.
 	public float RandomUpOrDown()
 	{
 		float num = UnityEngine.Random.value;
--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -3,9 +3,13 @@ using System;
 using System.Collections.Generic;
 
 public class PhotonManager : MonoBehaviour {
+	//Reference to GameManager
 	public GameManager GM;
+
+	//This queue holds references to all the photons. Makes it easy to know which photon is the 'current' one.
 	public Queue<PhotonData> PhotonQueue;
 
+	//Prefabs for each of the photons; for instantiation
 	public GameObject Red;
 	public GameObject Blue;
 	public GameObject Yellow;
@@ -13,24 +17,29 @@ public class PhotonManager : MonoBehaviour {
 	public GameObject Green;
 	public GameObject Orange;
 
+	//Half beats always come in pairs. This controls their triplet pattern
 	private bool triplet = false;
 	private int tripCount = 0;
 
-	private float BPS;
-	private System.Random rng;
+	//Used to make sure photons are spawned on beat
+	private float BPS; //Beats per second
 	private float generationCooldown;
 
 	// Use this for initialization
 	void Start () {
 		PhotonQueue = new Queue<PhotonData>();
 		BPS = GM.data.selectedSong.bpm/60;
+
+		//Set initial cooldown
 		setCooldown();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		//Subtract elapsed time from the cooldown timer.
 		generationCooldown -= Time.deltaTime;
 
+		//If the cooldown timer is over, generate a new photon
 		if(generationCooldown <= 0.0f)
 		{
 			PhotonQueue.Enqueue(generatePhoton());
@@ -41,6 +50,7 @@ public class PhotonManager : MonoBehaviour {
 
 	void setCooldown()
 	{
+		//Half beats always come in triplets; this if statement enforces that.
 		if(triplet)
 		{
 			generationCooldown = 0.5f/BPS;
@@ -54,6 +64,7 @@ public class PhotonManager : MonoBehaviour {
 			return;
 		}
 
+		//Randomly set to cooldown to fire a photon half a beat, one beat, or two beats from now.
 		float num = UnityEngine.Random.value;
 		if(num > 0.0f && num <= 0.3f)
 			generationCooldown = 1f/BPS;
@@ -66,10 +77,12 @@ public class PhotonManager : MonoBehaviour {
 			generationCooldown = 2/BPS;
 	}
 
+	//Randomly pick the next color photon to spawn; more likely to generate single colors than combos
 	PhotonData generatePhoton()
 	{
 		float num = UnityEngine.Random.value;
-		 
+
+		//Color decided and the photon itself is instantiated. The calling function puts it in the queue.
 		if(num > 0.0f && num <= 0.2f)
 			return new PhotonData(InputColor.RED, (GameObject) Instantiate(Red, this.transform.position, Quaternion.identity));
 		else if(num > 0.2f && num <= 0.4f)
